81516ef747165a05f6e9af6db6a4dffc557e1eec
jacoblinoflap <jebarcelona2020@plm.edu.ph>
2026-06-04 23:06:39 +0800

    Update Projects.jsx

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -147,10 +147,10 @@ function PhotoViewer({ project, onClose, current, total }) {
             {/* Fullscreen button */}
             <button
               onClick={() => setLightbox(true)}
-              className="absolute top-3 left-3 opacity-0 group-hover/photo:opacity-100 transition-opacity duration-200
-                text-white/70 hover:text-white bg-black/50 hover:bg-black/80 p-1.5 rounded-lg"
+              className="absolute top-3 left-3 opacity-100 lg:opacity-0 lg:group-hover/photo:opacity-100 transition-opacity duration-200
+                text-white bg-black/50 hover:bg-black/80 p-2 lg:p-1.5 rounded-lg"
               title="Full screen">
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
                 <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"/>
               </svg>
             </button>
